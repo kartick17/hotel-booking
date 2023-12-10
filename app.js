c62af60fa77df414adfe349c916b446b69c8ce05
ayanonline <ayanghosh1389@gmail.com>
2023-12-11 01:40:20 +0530

    response data changed

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const userRouter = require('./routes/userRoutes')
-const roomRouter = require('./routes/roomRoutes')
-const bookingRouter = require('./routes/bookingRoutes')
-const globalErrorHandeler = require('./controllers/errorController')
+const userRouter = require("./routes/userRoutes");
+const roomRouter = require("./routes/roomRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
+const globalErrorHandeler = require("./controllers/errorController");
 
-const app = express()
+const app = express();
 
-app.use(express.json({ limit: '10kb' }))
-app.use(express.urlencoded({ extended: true, limit: '10kb' }))
-app.use(cookieParser())
+const corsOptions = {
+  origin: [
+    // URL er last e kokhon o '/' debe na
+    "http://localhost:5173",
+  ],
+  credentials: true, // Allow credentials (cookies)
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+app.use(cors(corsOptions));
 
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/rooms', roomRouter)
-app.use('/api/v1/booking', bookingRouter)
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
 
-app.use(globalErrorHandeler)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/rooms", roomRouter);
+app.use("/api/v1/booking", bookingRouter);
 
-module.exports = app
+app.use(globalErrorHandeler);
+
+module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,70 +1,75 @@
-const bcrypt = require('bcrypt')
-const JWT = require('jsonwebtoken')
-const { User } = require('./../models')
-const catchAsync = require('../utils/catchAsync')
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+const { User } = require("./../models");
+const catchAsync = require("../utils/catchAsync");
 // require('dotenv').config()
 
 const createHashPassword = async (password) => {
-  return await bcrypt.hash(password, 10)
-}
+  return await bcrypt.hash(password, 10);
+};
 
 const createJWT = (data) => {
-  return JWT.sign({ email: data }, process.env.JWT_SECRET, { expiresIn: '30d' })
-}
+  return JWT.sign({ email: data }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 const comparePassword = async (password, dbPassword) => {
-  return await bcrypt.compare(password, dbPassword)
-}
+  return await bcrypt.compare(password, dbPassword);
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const { password, confirmPassword } = req.body
+  const { password, confirmPassword } = req.body;
 
   if (!password || password !== confirmPassword)
     return res.status(400).json({
       status: false,
-      message: 'Password and confirm password should be same!',
-    })
+      message: "Password and confirm password should be same!",
+    });
 
-  req.body.password = await createHashPassword(password)
+  req.body.password = await createHashPassword(password);
 
-  const user = await User.create(req.body)
+  const user = await User.create(req.body);
+
+  user.password = undefined;
 
   res.status(201).json({
     status: true,
     message: user,
-  })
-})
+  });
+});
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   if (!email || !password)
     return res.status(400).json({
       status: false,
-      message: 'All fields are required!',
-    })
+      message: "All fields are required!",
+    });
 
   const user = await User.findOne({
     where: { email },
-    attributes: ['id', 'password'],
-  })
+    // attributes: ["id", "password", "role"],
+  });
 
+  // console.log(user);
   if (!user || !(await comparePassword(password, user.password)))
     return res.status(401).json({
       status: false,
-      message: 'Invalid email or password!',
-    })
+      message: "Invalid email or password!",
+    });
 
-  const token = createJWT(email)
+  const token = createJWT(email);
 
-  res.cookie('jwt', token, {
+  res.cookie("jwt", token, {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-  })
+  });
 
+  user.password = undefined;
   res.status(200).json({
     status: true,
-    data: {
-      token,
-    },
-  })
-})
+    user,
+    // token,
+  });
+});
